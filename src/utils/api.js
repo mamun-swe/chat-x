a0c10ea32b5011a4564695159db0a3900b28f190
Mamun-swe <mhmamun166009@gmail.com>
2022-02-14 23:06:47 +0600

    ENDPOINT changes

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-// export const api = 'https://chat-x-api.herokuapp.com/api/v1/'
-export const api = 'http://localhost:4000/api/v1/'
+export const api = 'https://chat-x-api.herokuapp.com/api/v1/'
+// export const api = 'http://localhost:4000/api/v1/'
